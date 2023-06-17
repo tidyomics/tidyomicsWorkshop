5fb2efafab9369e09cb038c754a8b8cffb8ee10e
Stefano Mangiola <mangiolastefano@gmail.com>
2023-06-17 12:28:54 +1000

    add files

---
+++ R/data.R
@@ -0,0 +1,40 @@
+#'"seurat_obj"
+#'
+#' A Seurat dataset of single cell RNA sequencing data
+#'
+#'
+#' @format  A Seurat object.
+#' @usage data(seurat_obj)
+"seurat_obj"
+
+#' gate_seurat_obj
+#'
+#' Coordinates for a gate interactively drawn using tidygate
+#'
+#'
+#' @format  A list containing x,y coordinates for one gate
+#' @usage data(gate_seurat_obj)
+"gate_seurat_obj"
+
+
+#' seurat_obj_UMAP3
+#'
+#' A Seurat dataset of single cell RNA sequencing data with 3 UMAP dimesions
+#'
+#'
+#' @format  A Seurat object.
+#' @usage data(seurat_obj_UMAP3)
+"seurat_obj_UMAP3"
+
+#' pseudo_bulk
+#'
+#' A SummarizedExperiment object
+#'
+#' @description
+#' This object was saved only because leading to a strange failure of the github action, 
+#' while working perfectly in the local environment
+#' 
+#'
+#' @format  A SummarizedExperiment object.
+#' @usage data(pseudo_bulk)
+"pseudo_bulk"
