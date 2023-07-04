7d6d84e8e6b34caee81e23606c9ed8e055b48d9a
mikelove <michaelisaiahlove@gmail.com>
2023-07-04 15:08:09 +0200

    reorg to pass check

--- R/data_transcriptomics.R
+++ R/data_transcriptomics.R
@@ -1,30 +1,29 @@
-#'"seurat_obj"
-#'
-#' A Seurat dataset of single cell RNA sequencing data
-#'
-#'
-#' @format  A Seurat object.
-#' @usage data(seurat_obj)
-"seurat_obj"
+## #' seurat_obj
+## #'
+## #' A Seurat dataset of single cell RNA sequencing data
+## #'
+## #'
+## #' @format  A Seurat object.
+## #' @usage data(seurat_obj)
+## "seurat_obj"
 
-#' gate_seurat_obj
-#'
-#' Coordinates for a gate interactively drawn using tidygate
-#'
-#'
-#' @format  A list containing x,y coordinates for one gate
-#' @usage data(gate_seurat_obj)
-"gate_seurat_obj"
+## #' gate_seurat_obj
+## #'
+## #' Coordinates for a gate interactively drawn using tidygate
+## #'
+## #'
+## #' @format  A list containing x,y coordinates for one gate
+## #' @usage data(gate_seurat_obj)
+## "gate_seurat_obj"
 
-
-#' seurat_obj_UMAP3
-#'
-#' A Seurat dataset of single cell RNA sequencing data with 3 UMAP dimesions
-#'
-#'
-#' @format  A Seurat object.
-#' @usage data(seurat_obj_UMAP3)
-"seurat_obj_UMAP3"
+## #' seurat_obj_UMAP3
+## #'
+## #' A Seurat dataset of single cell RNA sequencing data with 3 UMAP dimesions
+## #'
+## #'
+## #' @format  A Seurat object.
+## #' @usage data(seurat_obj_UMAP3)
+## "seurat_obj_UMAP3"
 
 #' pseudo_bulk
 #'
@@ -39,7 +38,7 @@
 #' @usage data(pseudo_bulk)
 "pseudo_bulk"
 
-#'"sce_obj"
+#' sce_obj
 #'
 #' A sce dataset of single cell RNA sequencing data
 #'
@@ -57,7 +56,6 @@
 #' @usage data(gate_sce_obj)
 "gate_sce_obj"
 
-
 #' sce_obj_UMAP3
 #'
 #' A sce dataset of single cell RNA sequencing data with 3 UMAP dimesions
@@ -66,3 +64,12 @@
 #' @format  A sce object.
 #' @usage data(sce_obj_UMAP3)
 "sce_obj_UMAP3"
+
+#' theme_multipanel
+#'
+#' A file holding a theme object.
+#'
+#'
+#' @format theme
+#' @usage data(theme_multipanel)
+"theme_multipanel"
